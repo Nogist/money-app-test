fa7431c739b61fecb6e5459e81a14eee5781cad5
nogist <nogistune.me@gmaail.com>
2023-02-17 20:41:33 +0000

    Ui upload

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -5,10 +5,18 @@ const Form:React.FC = () => {
     <div className='form'>
       <form className='form__body'>
         <div className='form__body__header'>
-
+          <h1>Login to your dashboard</h1>
+          <h2>Provide details to login to your account</h2>
         </div>
         <div className='form__body__input'>
-
+          <span>
+            <label htmlFor=""></label>
+            <input type="text" />
+          </span>
+          <span>
+            <label htmlFor=""></label>
+            <input type="text" />
+          </span>
         </div>
         <div className='form__body__button'>
 
